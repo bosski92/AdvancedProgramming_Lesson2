06fa05d93526f1ff2c05fa0183ad8395bff9920d
bosski92 <pawel.jarzy@gmail.com>
2021-03-28 18:12:42 +0200

    Add project files.

---
+++ AdvancedProgramming_Lesson2/Data/MvcSeriesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AdvancedProgramming_Lesson2.Models;
+
+namespace AdvancedProgramming_Lesson2.Data
+{
+    public class MvcSeriesContext : DbContext
+    {
+        public MvcSeriesContext(DbContextOptions<MvcSeriesContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Series> Series { get; set; }
+
+    }
+}
---
+++ AdvancedProgramming_Lesson2/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvancedProgramming_Lesson2.Models
+
+{
+    public class Movie
+    {
+        [Display(Name = "ID")]
+        public int Id { get; set; }
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release date")]
+        public DateTime ReleaseDate { get; set; }
+        [Display(Name = "Genre")]
+        public string Genre { get; set; }
+        [Display(Name = "Price")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Director")]
+        public string Director { get; set; }
+        [Display(Name = "Oscar")]
+        public string Oscar { get; set; }
+
+    }
+}
